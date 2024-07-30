eef3523ef99c1c5204507b28a12f06d7944ac8e5
Ravi Nandan Ray <ravinandanray99@gmail.com>
2024-07-30 18:20:39 +0530

    added passport js authentication feature

--- controller/userController.js
+++ controller/userController.js
@@ -8,26 +8,6 @@ const user_login_get=(req,res)=>{
 const user_signup_get=(req,res)=>{
     res.render('authentication/signup', {title:'SignUp Form'});
 }
-const user_login_post=async(req,res)=>{
-    // checks if userName and password is valid, if not gives popup invalid username or password
-    // if valid loads the index page
-    const userName=req.body.userName;
-    const password=req.body.password;
-    const check = await User.findOne({ userName: userName });
-    if (!check) {
-        res.send("Username cannot be found");
-    }
-    else{
-        // Compare the hashed password from the database with the plaintext password
-        const isPasswordMatch =await bcrypt.compare(password, check.password);
-        if (!isPasswordMatch) {
-            res.send("wrong Password");
-        }
-        else {
-            res.redirect('/');
-        }
-    }
-}
 
 const user_signup_post=async(req,res)=>{
     const exists=await User.findOne({userName: req.body.userName});
@@ -35,8 +15,6 @@ const user_signup_post=async(req,res)=>{
         res.send('Username already exists, go back and try with different username');
     }
     else{
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        req.body.password = hashedPassword; // Replace the original password with the hashed one
         const user=new User(req.body);
         user.save()
             .then((result)=>{
@@ -47,9 +25,22 @@ const user_signup_post=async(req,res)=>{
             });
     }
 }
+
+const user_loggedIn=async(req,res)=>{
+    res.redirect('/');
+}
+
+const user_loggedOut=(req,res,next)=>{
+    req.logOut((err)=>{
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+}
+
 module.exports={
     user_login_get,
     user_signup_get,
-    user_login_post,
-    user_signup_post
+    user_signup_post,
+    user_loggedIn,
+    user_loggedOut
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport'); // required passport for authentication stretegy
+const expressSession=require('express-session');
+const {initialisingPassport,isAuthenticated}=require('../passportConfig.js');
 const  userController=require('../controller/userController');
 const User=require('../models/user');
+router.use(expressSession({secret:'secret', resave:'false', saveUninitialized:'false'}));
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.get('/login',userController.user_login_get);
 router.get('/signup',userController.user_signup_get);
-router.post('/login',userController.user_login_post);
+router.post('/login',passport.authenticate('local',{failureRedirect:'/signup',failureMessage:true}),userController.user_loggedIn);
 router.post('/signup',userController.user_signup_post);
+router.post('/logout',isAuthenticated,userController.user_loggedOut);
 
 module.exports=router;
